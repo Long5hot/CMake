98f120bdd836a02408af865199e3dee8b548e569
krushnparmar <krushnparmar@gmail.com>
2022-11-01 11:55:37 +0530

    Basic CMake

---
+++ basicCMake/source/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<concepts>
+
+template <typename T>
+requires std::integral<T>
+T add( T a , T b) {
+    return a+b;
+}
+
+int main() {
+
+    std::cout << "Hello C++ 20 with CMake on Linux" << std::endl;
+    std::cout << "The Sum of a + b = " << add(7,3) << std::endl;
+    return 0;
+}
